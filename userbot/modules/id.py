94d09df93f5b86dadeb50aa6bf60606c601d7ac7
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-31 16:13:34 +0700

    Update id.py

--- userbot/modules/id.py
+++ userbot/modules/id.py
@@ -11,13 +11,13 @@ async def image_maker(event):
         replied_user.from_id, file="user.png", download_big=True
     )
     user_photo = Image.open("user.png")
-    id_template = Image.open("userbot/resources/FrameID.png")
+    id_template = Image.open("resources/extras/Kaisar.png")
     user_photo = user_photo.resize((989, 1073))
     id_template.paste(user_photo, (1229, 573))
     position = (2473, 481)
     draw = ImageDraw.Draw(id_template)
     color = "rgb(23, 43, 226)"  # red color
-    font = ImageFont.truetype("userbot/resources/font.ttf", size=200)
+    font = ImageFont.truetype("resources/fonts/1.ttf", size=200)
     draw.text(
         position,
         replied_user.sender.first_name.replace("\u2060", ""),
